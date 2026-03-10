bc0d26a98a5e5a8f734d82168c05ad5234da98a2
Nicolai <nico368f@gmail.com>
2026-03-10 09:34:51 +0100

    Added base for a card controller

---
+++ MTG-Emulator.Backend/Controllers/CardsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTG_Emulator.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+
+    }
+}
