6c4a4c683e6f401e8d4f9f7c1a26aa614c735877
Dani211e <daniel.jenrich@gmail.com>
2026-03-11 15:52:06 +0100

    Implement bulk card download for seeding

--- MTG-Emulator.Backend/Program.cs
+++ MTG-Emulator.Backend/Program.cs
@@ -1,8 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using MTG_Emulator.Backend.DB;
 using MTG_Emulator.Backend.DB.Models;
-using Scalar.AspNetCore;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+using (var db = new MTGContext())
+{
+    seedDb(db);
+}
 
 void seedDb(MTGContext db)
 {
@@ -21,28 +25,41 @@ void seedDb(MTGContext db)
 
 
 }
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+/*builder.Services.AddOpenApi();
 
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<MTGContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<MTGContext>();
-    seedDb(db);
-}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
-app.MapControllers();
-app.MapOpenApi();
-app.MapScalarApiReference();
+app.UseHttpsRedirection();
+
+string[] summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
 
 app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}*/
