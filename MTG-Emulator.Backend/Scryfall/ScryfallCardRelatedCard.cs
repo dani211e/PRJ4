9a757b9f6bf097dcadf87c851d51c04409e845d3
Dani211e <daniel.jenrich@gmail.com>
2026-03-17 11:57:25 +0100

    formatting

--- MTG-Emulator.Backend/Scryfall/ScryfallCardRelatedCard.cs
+++ MTG-Emulator.Backend/Scryfall/ScryfallCardRelatedCard.cs
@@ -4,17 +4,22 @@ namespace MTG_Emulator.Backend.Scryfall
 {
     public struct ScryfallCardRelatedCard
     {
-        [JsonPropertyName("id")] public Guid Id { get; set; }
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
 
         //[JsonPropertyName("object")]
         //public string CardObject { get; set; }
 
-        [JsonPropertyName("component")] public string Component { get; set; }
+        [JsonPropertyName("component")]
+        public string Component { get; set; }
 
-        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
 
-        [JsonPropertyName("type_line")] public string TypeLine { get; set; }
+        [JsonPropertyName("type_line")]
+        public string TypeLine { get; set; }
 
-        [JsonPropertyName("uri")] public string Uri { get; set; }
+        [JsonPropertyName("uri")]
+        public string Uri { get; set; }
     }
 }
