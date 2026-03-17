b881472a5c0223e7512f724c36746bce02c103e4
Dani211e <daniel.jenrich@gmail.com>
2026-03-17 08:43:22 +0100

    Move db functions to helper class
    also make program.cs an actual main func

---
+++ MTG-Emulator.Backend/DbHelper.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
+using MTG_Emulator.Backend.DB;
+using MTG_Emulator.Backend.DB.Models;
+using MTG_Emulator.Backend.Scryfall;
+
+namespace MTG_Emulator.Backend
+{
+    public static class DbHelper
+    {
+        private const string download_file_name = "ScryfallBulkCards.json";
+        private static readonly string path = Path.Combine(Path.GetTempPath(), download_file_name);
+
+        public static async Task SeedDb(MTGContext db, HttpClient client, int count = 1)
+        {
+            if (!File.Exists(path) || File.GetCreationTime(path).Date < DateTime.Today.Date)
+                await downloadBulkCardsAsync(client);
+
+            if (path.IsNullOrEmpty())
+                throw new FileNotFoundException($@"File not found at: {path}");
+
+            var cards = await readScryfallCards(path!).Take(count).Select(x => x.ToCard()).ToListAsync();
+
+            var player1 = new Player
+            {
+                Username = "Kasper",
+                Password = "Loser",
+                GamesWon = 0,
+                GamesLost = 1242,
+                GamesDrawed = 2,
+            };
+
+            var deck1 = new Deck
+            {
+                DeckName = "Best deck ever", Cards = cards, DeckCommander = cards[0].Name,
+                Player = player1,
+            };
+
+            db.AddRange(cards);
+            db.Add(player1);
+            db.Add(deck1);
+            await db.SaveChangesAsync();
+        }
+
+        private static async Task downloadBulkCardsAsync(HttpClient client)
+        {
+            string baseUrl = $@"https://api.scryfall.com/bulk-data";
+            // We need to make an initial request that tells us where the actual download url is located
+            Stream s1 = await client.GetStreamAsync(baseUrl);
+            ScryfallBulkResponse r = await JsonSerializer.DeserializeAsync<ScryfallBulkResponse>(s1);
+
+            string? downloadUrl = r.Data.First(x => x.Type == "oracle_cards").DownloadUri.ToString();
+            if (downloadUrl.IsNullOrEmpty())
+                return;
+
+            Stream s2 = await client.GetStreamAsync(downloadUrl);
+            using var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            await s2.CopyToAsync(fs);
+        }
+
+        private static async IAsyncEnumerable<ScryfallCard> readScryfallCards(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File not found at path: {path}");
+
+            await using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var opt = new JsonSerializerOptions { UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip };
+
+            await foreach (var card in JsonSerializer.DeserializeAsyncEnumerable<ScryfallCard>(fs, opt))
+                yield return card;
+        }
+    }
+}
--- MTG-Emulator.Backend/Program.cs
+++ MTG-Emulator.Backend/Program.cs
@@ -1,65 +1,57 @@
+using System.Net.Http.Headers;
 using MTG_Emulator.Backend.DB;
-using MTG_Emulator.Backend.DB.Models;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-using (var db = new MTGContext())
+namespace MTG_Emulator.Backend
 {
-    seedDb(db);
-}
-
-void seedDb(MTGContext db)
-{
-    var c1 = new Card { Name = "Test", OracleText = "Very good card", ImageURI = "https://cards.scryfall.io/large/front/a/9/a9b2a843-c6fe-4d19-801e-1538e4381ab0.jpg?1764119928"};
-    var c2 = new Card { Name = "Good card", OracleText = "Even better card", ImageURI = "https://cards.scryfall.io/large/front/8/2/829d91e9-4878-4e55-a262-ac0d55b65d4e.jpg?1764119935"};
-
-    var player1 = new Player {Username = "Kasper", Password = "1234", GamesWon = 4, GamesLost = 0, GamesDrawed = 2 };
-
-    var deck1 = new Deck { DeckName = "Best deck ever", Cards = new List<Card>{c1,c1,c1,c2,c2,c2,c2}, DeckCommander = c1.Name, Player = player1};
-
-    db.Add(c1);
-    db.Add(c2);
-    db.Add(player1);
-    db.Add(deck1);
-    db.SaveChanges();
-
-
-}
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-/*builder.Services.AddOpenApi();
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) app.MapOpenApi();
-
-app.UseHttpsRedirection();
-
-string[] summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    internal abstract class Program
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+        public static async Task Main(string[] args)
+        {
+            //var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+            using HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}*/
+            await using var db = new MTGContext();
+            await DbHelper.SeedDb(db, httpClient, 20);
+
+            /*
+            // Add services to the container.
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            /*builder.Services.AddOpenApi();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) app.MapOpenApi();
+
+            app.UseHttpsRedirection();
+
+            string[] summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", () =>
+                {
+                    var forecast = Enumerable.Range(1, 5).Select(index =>
+                            new WeatherForecast
+                            (
+                                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                Random.Shared.Next(-20, 55),
+                                summaries[Random.Shared.Next(summaries.Length)]
+                            ))
+                        .ToArray();
+                    return forecast;
+                })
+                .WithName("GetWeatherForecast");
+
+            app.Run();
+            */
+        }
+    }
+}
