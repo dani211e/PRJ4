208762542fe3948666a6af2c099c8deda7bf22a9
Nicolai <nico368f@gmail.com>
2026-03-17 08:07:35 +0100

    Added a Unit tets project

---
+++ UnitTests/Backend/DeckController.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests.Backend
+{
+    public class DeckController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ UnitTests/Backend/TokenController.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests.Backend
+{
+    public class TokenController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
