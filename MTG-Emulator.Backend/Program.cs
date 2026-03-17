735a335c29dfd8cf7c39aaca0647ee11becff723
Dani211e <daniel.jenrich@gmail.com>
2026-03-17 09:24:28 +0100

    use main funct

--- MTG-Emulator.Backend/Program.cs
+++ MTG-Emulator.Backend/Program.cs
@@ -4,30 +4,36 @@ using MTG_Emulator.Backend;
 using MTG_Emulator.Backend.DB;
 using Scalar.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<MTGContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+internal abstract class Program
 {
-    using HttpClient httpClient = new HttpClient();
-    httpClient.DefaultRequestHeaders.Clear();
-    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    var db = scope.ServiceProvider.GetRequiredService<MTGContext>();
-    await DbHelper.SeedDb(db, httpClient);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddOpenApi();
+
+        builder.Services.AddDbContext<MTGContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            using HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var db = scope.ServiceProvider.GetRequiredService<MTGContext>();
+            await DbHelper.SeedDb(db, httpClient);
+        }
+
+        app.MapControllers();
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+
+        app.Run();
+    }
 }
-
-app.MapControllers();
-app.MapOpenApi();
-app.MapScalarApiReference();
-
-app.Run();
