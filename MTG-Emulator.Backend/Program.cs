accd8d9dfe0d1b6c75544fd4867c56b96046c1d1
Dani211e <daniel.jenrich@gmail.com>
2026-03-17 09:15:24 +0100

    Merge resolution

--- MTG-Emulator.Backend/Program.cs
+++ MTG-Emulator.Backend/Program.cs
@@ -1,57 +1,33 @@
 using System.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore;
+using MTG_Emulator.Backend;
 using MTG_Emulator.Backend.DB;
+using Scalar.AspNetCore;
 
-namespace MTG_Emulator.Backend
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<MTGContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
-    internal abstract class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            //var builder = WebApplication.CreateBuilder(args);
+    using HttpClient httpClient = new HttpClient();
+    httpClient.DefaultRequestHeaders.Clear();
+    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            using HttpClient httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-            await using var db = new MTGContext();
-            await DbHelper.SeedDb(db, httpClient);
-
-            /*
-            // Add services to the container.
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            /*builder.Services.AddOpenApi();
-
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment()) app.MapOpenApi();
-
-            app.UseHttpsRedirection();
-
-            string[] summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", () =>
-                {
-                    var forecast = Enumerable.Range(1, 5).Select(index =>
-                            new WeatherForecast
-                            (
-                                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                                Random.Shared.Next(-20, 55),
-                                summaries[Random.Shared.Next(summaries.Length)]
-                            ))
-                        .ToArray();
-                    return forecast;
-                })
-                .WithName("GetWeatherForecast");
-
-            app.Run();
-            */
-        }
-    }
+    var db = scope.ServiceProvider.GetRequiredService<MTGContext>();
+    await DbHelper.SeedDb(db, httpClient);
 }
+
+app.MapControllers();
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.Run();
