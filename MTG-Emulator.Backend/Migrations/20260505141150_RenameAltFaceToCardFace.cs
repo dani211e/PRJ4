c2a2bc851cb859d6f6cf28a66b7dfc9f04a44715
Nicolai <nico368f@gmail.com>
2026-05-05 16:15:32 +0200

    Added a migration for changing AltFace to CardFace

---
+++ MTG-Emulator.Backend/Migrations/20260505141150_RenameAltFaceToCardFace.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MTG_Emulator.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAltFaceToCardFace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "URI",
+                table: "RelatedCards",
+                newName: "ImageUri");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageURI",
+                table: "AltFaces",
+                newName: "ImageUri");
+
+            migrationBuilder.RenameColumn(
+                name: "AltFaceId",
+                table: "AltFaces",
+                newName: "CardFaceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUri",
+                table: "RelatedCards",
+                newName: "URI");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUri",
+                table: "AltFaces",
+                newName: "ImageURI");
+
+            migrationBuilder.RenameColumn(
+                name: "CardFaceId",
+                table: "AltFaces",
+                newName: "AltFaceId");
+        }
+    }
+}
