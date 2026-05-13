c94ab00ff0df8dd5b607a2cc0007c39fa158af82
Nicolai <nico368f@gmail.com>
2026-05-13 10:05:16 +0200

    Fixed the problem with string lenght for ApiUserId

--- MTG-Emulator.Backend/Migrations/20260513075343_FixApiUserIdColumnLength.Designer.cs
+++ MTG-Emulator.Backend/Migrations/20260513075343_FixApiUserIdColumnLength.Designer.cs
@@ -4,6 +4,7 @@ using MTG_Emulator.Backend.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MTG_Emulator.Backend.Migrations
 {
     [DbContext(typeof(MTGContext))]
-    partial class MTGContextModelSnapshot : ModelSnapshot
+    [Migration("20260513075343_FixApiUserIdColumnLength")]
+    partial class FixApiUserIdColumnLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
