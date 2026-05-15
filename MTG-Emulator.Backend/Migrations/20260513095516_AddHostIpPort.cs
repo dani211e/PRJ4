b687e0a34dc64f0dabf0eb7209d726701121c080
Johan Høffner <202409238@post.au.dk>
2026-05-15 10:36:24 +0200

    mirror added th unity, for multiplayer

---
+++ MTG-Emulator.Backend/Migrations/20260513095516_AddHostIpPort.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MTG_Emulator.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHostIpPort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HostIp",
+                table: "Games",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "HostPort",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HostIp",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "HostPort",
+                table: "Games");
+        }
+    }
+}
