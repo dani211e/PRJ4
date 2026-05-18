d49b427c4dbdbd5a272e930a1a7f6e97353b7a72
Dani211e <daniel.jenrich@gmail.com>
2026-05-18 14:10:39 +0200

    Add object manager
    Keeps track of per player objects that might interact with events (like
    move events).
    We need to be able to differentiate identical cards, and scryfall id,
    position and name aren't reliable or unique enough.

---
+++ MTG-Emulator/Assets/Scripts/CardManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MTG_Emulator.Unity.Synchronization.Events;
+using UnityEngine;
+
+namespace MTG_Emulator
+{
+    public class CardManager : MonoBehaviour
+    {
+        // An array of dictionaries where each index represents a player in the game,
+        // the key is an object's identifier (e.g Card.Identifier) which is received from events
+        private static Dictionary<Guid, GameObject>[] cards;
+
+        [SerializeField]
+        private GameObject cardPrefab;
+
+        public void Awake()
+        {
+            // Krilling my shelf for making this abomination of an initializer
+            cards = new Dictionary<Guid, GameObject>[GameSession.MaxPlayers]
+                .Select(_ => new Dictionary<Guid, GameObject>()).ToArray();
+        }
+
+        public static void AddObject(int playerIndex, Guid identifier, GameObject obj)
+        {
+            if (!cards[playerIndex].TryAdd(identifier, obj))
+                return;
+
+            if (playerIndex != GameSession.PlayerId)
+                return;
+
+            SignalRClient.Instance.Broadcast(new NewCardEvent(playerIndex, identifier)
+            {
+                Position = obj.transform.position.ToSystem2(),
+            });
+            Debug.Log("Sent new card event");
+        }
+
+        public static GameObject Get(int playerIndex, Guid identifier) =>
+            cards[playerIndex].GetValueOrDefault(identifier);
+    }
+}
