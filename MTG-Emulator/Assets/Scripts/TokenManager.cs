541433f8e4d1a1ce42372a0b6582c96fc199d7a1
Christopher F. Jørgensen <202300791@post.au.dk>
2026-05-19 11:40:30 +0200

    created token.cs and cardmenu which handles spawning of tokens and opening a menu where you can spawn tokens from
    small fix for creating a token
    
    card menu now instantiates a new button foreach token a card is able to create
    
    small fix

---
+++ MTG-Emulator/Assets/Scripts/TokenManager.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+using MTG_Emulator.Backend.DB.Models;
+using MTG_Emulator.Unity.Db.DTO.CardDTO;
+using MTG_Emulator.Unity.Db.DTO.RelatedCardDTO;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+public class TokenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private Transform battlefieldParent;
+    private TMP_Text cardName;
+
+
+    private CardDto cardData;
+    private Button button;
+    private bool Istapped = false;
+    private CardZonesTypes currentzone;
+    /*private void OnGUI()
+    {
+        Event rightClick =  Event.current;
+        if (rightClick.button == 1 && rightClick.isMouse)
+        {
+            Card card = 
+            RelatedCardDto token = card.RelatedCards[0];
+            GameObject tokenObj = Instantiate(cardPrefab, battlefieldParent);
+            Token tokenScript = tokenObj.GetComponent<Token>();
+            tokenScript.Setup(token);
+            Debug.Log(token.Name + "Has been summoned");
+        }
+    }*/
+}
