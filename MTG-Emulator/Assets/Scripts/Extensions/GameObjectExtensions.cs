223ce2a0d6b3a2988183c95f8cc852e95e5f3ef7
Dani211e <daniel.jenrich@gmail.com>
2026-05-20 17:43:27 +0200

    Add RemoveComponent extension for gameobject

---
+++ MTG-Emulator/Assets/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MTG_Emulator.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static void RemoveComponent<T>(this GameObject obj) where T : MonoBehaviour
+        {
+            var comp = obj.GetComponent<T>();
+            
+            if(!comp)
+                Debug.LogWarning($"Attempted to remove component {nameof(T)} from {obj.name}, but it was not found!");
+
+            Object.Destroy(comp);
+        }
+    }
+}
