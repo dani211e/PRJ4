6e35d43b48fb8cc3aff317d415d45c890d393b1a
Nicolai <nico368f@gmail.com>
2026-05-21 14:40:01 +0200

    Created a GameResultPopup UI

---
+++ MTG-Emulator/Assets/Scripts/GameResultPopup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameResultPopup : MonoBehaviour
+    {
+        public void OnClickReportResult(int gameResult)
+        {
+            Debug.Log(gameResult);
+            StartCoroutine(APIManager.Instance.UpdatePlayerStats(gameResult,
+                s => {}, 
+                error => {Debug.LogError("Failed to update player stats " + error);}));
+            
+            LeaveGame();
+        }
+
+        public void LeaveGame()
+        {
+            
+            
+            Debug.Log("Leaving game");
+        }
+    }
+}
