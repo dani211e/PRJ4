a2766a5f03748f34f92bf8e9d1ac9a6b799877b0
Nicolai <nico368f@gmail.com>
2026-05-21 14:38:41 +0200

    Created a PopupManager

---
+++ MTG-Emulator/Assets/Scripts/PopupManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PopupManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject settingsPopup;
+        [SerializeField] private GameObject gameResultPopup;
+        
+        
+        public void CloseAllPopups()
+        {
+            if (settingsPopup != null)
+                settingsPopup.SetActive(false);
+
+            if (gameResultPopup != null)
+                gameResultPopup.SetActive(false);
+
+        }
+        
+        public void OpenSettingsPopup()
+        {
+            if (settingsPopup != null)
+                settingsPopup.SetActive(true);
+        }
+        
+        public void OpenGameResultPopup()
+        {
+            if (gameResultPopup != null)
+                gameResultPopup.SetActive(true);
+        }
+        
+    }
+}
