85ffb4417289d7d5f0d74134b0a0027b08f39114
Johan Høffner <202409238@post.au.dk>
2026-05-22 15:14:33 +0200

    Made mutiple select with drag and shift click

---
+++ MTG-Emulator/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,151 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+    public static SelectionManager Instance;
+
+    [SerializeField] private RectTransform selectionBox;
+    [SerializeField] private Canvas canvas;
+
+    private Vector2 startPos;
+    private bool isDragging = false;
+    private List<Tapable> selectedCards = new();
+
+    private void Awake()
+    {
+        Instance = this;
+        selectionBox.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && HasSelection)
+        {
+            foreach (Tapable t in new List<Tapable>(selectedCards))
+                t.TapToggle();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Check if we clicked directly on a Tapable
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition
+            };
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            bool hitCard = results.Exists(r => r.gameObject.GetComponent<Tapable>() != null 
+                                               || r.gameObject.GetComponentInParent<Tapable>() != null);
+
+            if (!hitCard)
+            {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                    DeselectAll();
+
+                startPos = Input.mousePosition;
+                isDragging = true;
+                selectionBox.gameObject.SetActive(true);
+            }
+        }
+        else if (Input.GetMouseButton(0) && isDragging)
+        {
+            UpdateSelectionBox(Input.mousePosition);
+            // Highlight cards inside box while dragging
+            HighlightCardsInBox();
+        }
+        else if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+            selectionBox.gameObject.SetActive(false);
+
+            float dragSize = Vector2.Distance(startPos, Input.mousePosition);
+            if (dragSize < 5f)
+            { 
+                return;
+            }
+
+            // Finalize selection
+            SelectCardsInBox();
+        }
+    }
+
+    private void UpdateSelectionBox(Vector2 currentPos)
+    {
+        Vector2 size = new Vector2(
+            Mathf.Abs(currentPos.x - startPos.x),
+            Mathf.Abs(currentPos.y - startPos.y)
+        );
+
+        Vector2 origin = new Vector2(
+            Mathf.Min(startPos.x, currentPos.x),
+            Mathf.Min(startPos.y, currentPos.y)
+        );
+
+        selectionBox.position = new Vector3(origin.x + size.x / 2, origin.y + size.y / 2, 0);
+        selectionBox.sizeDelta = size / canvas.scaleFactor;
+    }
+
+    private void HighlightCardsInBox()
+    {
+        Rect selectionRect = new Rect(
+            Mathf.Min(startPos.x, Input.mousePosition.x),
+            Mathf.Min(startPos.y, Input.mousePosition.y),
+            Mathf.Abs(Input.mousePosition.x - startPos.x),
+            Mathf.Abs(Input.mousePosition.y - startPos.y)
+        );
+
+        foreach (Tapable tapable in FindObjectsOfType<Tapable>())
+        {
+            Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, tapable.transform.position);
+            if (selectionRect.Contains(screenPos))
+                tapable.HighLight();
+            else
+                tapable.UnHighlight();
+        }
+    }
+
+    private void SelectCardsInBox()
+    {
+        Rect selectionRect = new Rect(
+            Mathf.Min(startPos.x, Input.mousePosition.x),
+            Mathf.Min(startPos.y, Input.mousePosition.y),
+            Mathf.Abs(Input.mousePosition.x - startPos.x),
+            Mathf.Abs(Input.mousePosition.y - startPos.y)
+        );
+
+        foreach (Tapable tapable in FindObjectsOfType<Tapable>())
+        {
+            Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, tapable.transform.position);
+            if (selectionRect.Contains(screenPos))
+                tapable.Select();
+            else if (!Input.GetKey(KeyCode.LeftShift))
+                tapable.Deselect();
+        }
+    }
+
+    public void DeselectAll()
+    {
+        foreach (Tapable t in new List<Tapable>(selectedCards))
+            t.Deselect();
+        selectedCards.Clear();
+    }
+
+    public void Register(Tapable tapable)
+    {
+        if (!selectedCards.Contains(tapable))
+            selectedCards.Add(tapable);
+    }
+
+    public void Unregister(Tapable tapable)
+    {
+        selectedCards.Remove(tapable);
+    }
+
+    public bool HasSelection => selectedCards.Count > 0;
+    public List<Tapable> SelectedCards => selectedCards;
+}
--- MTG-Emulator/Assets/Scripts/Tapable.cs
+++ MTG-Emulator/Assets/Scripts/Tapable.cs
@@ -1,12 +1,26 @@
 using MTG_Emulator.Unity.Synchronization.Enums;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Tapable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Tapable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private bool isTapped = false;
     private bool isHovered = false;
+    private bool isSelected = false;
     private ZoneType currentZone;
+    private Outline selectionOutline;
+
+    private void Awake()
+    {
+        selectionOutline = GetComponent<Outline>();
+        if (selectionOutline == null)
+            selectionOutline = gameObject.AddComponent<Outline>();
+
+        selectionOutline.enabled = false;
+        selectionOutline.effectColor = Color.yellow;
+        selectionOutline.effectDistance = new Vector2(3, 3);
+    }
 
     public void SetZone(ZoneType zone)
     {
@@ -17,21 +31,68 @@ public class Tapable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (currentZone != ZoneType.Bf || !isHovered)
+            if (currentZone != ZoneType.Bf)
                 return;
 
-            transform.Rotate(0, 0, isTapped ? 90.0f : -90.0f);
-            isTapped = !isTapped;
+            if (SelectionManager.Instance.HasSelection)
+                return;
+
+            if (!isHovered)
+                return;
+
+            TapToggle();
         }
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void TapToggle()
     {
-        isHovered = true;
+        transform.Rotate(0, 0, isTapped ? 90.0f : -90.0f);
+        isTapped = !isTapped;
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void HighLight()
     {
-        isHovered = false;
+        selectionOutline.enabled = true;
+        selectionOutline.effectColor = Color.cyan;
+    }
+
+    public void UnHighlight()
+    {
+        if (!isSelected)
+            selectionOutline.enabled = false;
+        else
+            selectionOutline.effectColor = Color.yellow;
+    }
+
+    public void Select()
+    {
+        isSelected = true;
+        selectionOutline.enabled = true;
+        selectionOutline.effectColor = Color.yellow;
+        SelectionManager.Instance.Register(this);
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+        selectionOutline.enabled = false;
+        SelectionManager.Instance.Unregister(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) => isHovered = true;
+    public void OnPointerExit(PointerEventData eventData) => isHovered = false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (isSelected)
+                Deselect();
+            else
+                Select();
+        }
     }
 }
