7a29758e0fd83a097fbfa2f6b9e1c448ec4d48aa
Nicolai <nico368f@gmail.com>
2026-05-22 10:44:51 +0200

    updated the authentication DTOs

--- MTG-Emulator.Unity/Db/DTO/AuthenticationDTO/LoginDto.cs
+++ MTG-Emulator.Unity/Db/DTO/AuthenticationDTO/LoginDto.cs
@@ -1,7 +1,13 @@
-﻿namespace MTG_Emulator.Unity.Db.DTO.AuthenticationDTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTG_Emulator.Unity.Db.DTO.AuthenticationDTO;
 
 public class LoginDto
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
--- MTG-Emulator.Unity/Db/DTO/AuthenticationDTO/ResetPasswordDto.cs
+++ MTG-Emulator.Unity/Db/DTO/AuthenticationDTO/ResetPasswordDto.cs
@@ -1,9 +1,17 @@
-﻿namespace MTG_Emulator.Unity.Db.DTO.AuthenticationDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTG_Emulator.Unity.Db.DTO.AuthenticationDTO
 {
     public class ResetPasswordDto
     {
         public string? TargetUserId { get; set; }
+
+        [Required]
+        [MinLength(8)]
         public string NewPassword { get; set; } = string.Empty;
+
+        [Required]
+        [Compare(nameof(NewPassword))]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
