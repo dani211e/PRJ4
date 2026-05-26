cc8930f9f3f0a880eefb1b899755294c22e29e13
Dani211e <daniel.jenrich@gmail.com>
2026-05-26 22:40:24 +0200

    Clamp battlefield movement to within zone bounds

--- MTG-Emulator/Assets/Scripts/BattlefieldDrop.cs
+++ MTG-Emulator/Assets/Scripts/BattlefieldDrop.cs
@@ -1,3 +1,4 @@
+using System;
 using MTG_Emulator.Extensions;
 using MTG_Emulator.Unity.Synchronization.Enums;
 using MTG_Emulator.Unity.Synchronization.Events;
@@ -8,6 +9,20 @@ public class FreeDropZone : MonoBehaviour, IDropHandler
 {
     [SerializeField]
     private ZoneType zoneType;
+
+    private float left;
+    private float right;
+    private float top;
+    private float bottom;
+
+    private void Awake()
+    {
+        var zoneRect = ((RectTransform)transform).rect;
+        left = zoneRect.xMin;
+        right = zoneRect.xMax;
+        top = zoneRect.yMin;
+        bottom = zoneRect.yMax;
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -27,6 +42,17 @@ public class FreeDropZone : MonoBehaviour, IDropHandler
         dragged.transform.SetAsLastSibling();
         dragged.transform.localScale = Vector3.one;
         dragged.transform.localRotation = Quaternion.identity;
+
+        // Make sure card stays within bounds of zone
+        // TODO: Make card size an easily accessible property, this is annoying to do.
+        var cardSize = ((RectTransform)dragged.transform).sizeDelta;
+        var cardW = cardSize.x / 2;
+        var cardH = cardSize.y / 2;
+
+        var clampedX = Math.Clamp(dragged.transform.localPosition.x, left + cardW, right - cardW);
+        var clampedY = Math.Clamp(dragged.transform.localPosition.y, top + cardH, bottom - cardH);
+
+        dragged.transform.localPosition = new Vector3(clampedX, clampedY, 0);
 
         var card = dragged.GetComponent<Card>();
         if (card != null)
